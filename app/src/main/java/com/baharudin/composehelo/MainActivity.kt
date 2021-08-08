1581460646089f3cf1a9430f910b4a48f5a43ba5
Baharudin78 <bencoxgame78@gmail.com>
2021-08-08 17:05:21 +0700

    fix

--- app/src/main/java/com/baharudin/composehelo/MainActivity.kt
+++ app/src/main/java/com/baharudin/composehelo/MainActivity.kt
@@ -3,45 +3,33 @@ package com.baharudin.composehelo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.baharudin.composehelo.ui.HomeScreen
+import com.baharudin.composehelo.ui.theme.ComposeHeloTheme
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
-            BackScreen()
+            ComposeHeloTheme {
+                HomeScreen()
+            }
         }
     }
-}
-@Composable
-fun BackScreen() {
-    Surface() {
-        Column(
-            modifier = Modifier.padding(5.dp)
-                .fillMaxHeight()
-                .fillMaxWidth()
-
-        ) {
-
-
-        }
-    }
-}
-
-
-@Preview
-@Composable
-fun PreviewUcapan() {
-    BackScreen()
 }
--- app/src/main/java/com/baharudin/composehelo/ui/theme/Type.kt
+++ app/src/main/java/com/baharudin/composehelo/ui/theme/Type.kt
@@ -1,28 +1,44 @@
 package com.baharudin.composehelo.ui.theme
 
 import androidx.compose.material.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.baharudin.composehelo.R
+
+// Set of Material typography styles to start with
+
+val gothicA1 = FontFamily(
+    listOf(
+        Font(R.font.gothic_reguler, FontWeight.Normal),
+        Font(R.font.gothic_reguler, FontWeight.Medium),
+        Font(R.font.gothic_reguler, FontWeight.SemiBold),
+        Font(R.font.gothic_reguler, FontWeight.Bold),
+        Font(R.font.gothic_reguler, FontWeight.Black),
+    )
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        color = AquaBlue,
+        fontFamily = gothicA1,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
         fontSize = 14.sp
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+    h1 = TextStyle(
+        color = TextWhite,
+        fontFamily = gothicA1,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp
+    ),
+    h2 = TextStyle(
+        color = TextWhite,
+        fontFamily = gothicA1,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp
     )
-    */
 )
